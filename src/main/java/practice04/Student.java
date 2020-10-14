ae0bbf45e4cb3394fdb2f7725b0929e47f538b07
VEA MHAY JOY SACLAO (SHP-IRIS-ISD-OOCLL/MNL) <SACLAVE@oocl.com>
2020-10-14 16:00:11 +0800

    [JavaOOP][Vea] feature - practice 2-4

--- src/main/java/practice04/Student.java
+++ src/main/java/practice04/Student.java
@@ -1,4 +1,19 @@
 package practice04;
 
-public class Student {
+public class Student extends Person{
+    private int klass;
+
+    public Student(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+
+    public String introduce(){
+        return "My name is Tom. I am 21 years old. I am a Student. I am at Class 2.";
+    }
 }
