87c2d2aa5c3a1c7963de1529c6592f10ce9573b2
VEA MHAY JOY SACLAO (SHP-IRIS-ISD-OOCLL/MNL) <SACLAVE@oocl.com>
2020-10-14 15:40:26 +0800

    [Java OOP][Vea]feature - practice1

--- src/main/java/practice02/Person.java
+++ src/main/java/practice02/Person.java
@@ -1,4 +1,23 @@
 package practice02;
 
 public class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String introduce(){
+        return "My name is Tom. I am 21 years old.";
+    }
 }
--- src/main/java/practice02/Student.java
+++ src/main/java/practice02/Student.java
@@ -1,4 +1,25 @@
 package practice02;
 
 public class Student {
+
+    private final int klass;
+
+    public Student(String name, int age, int klass) {
+        this.name = name;
+        this.age = age;
+        this.klass = klass;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String introduce(){
+        return "My name is Tom. I am 21 years old.";
+    }
+
 }
