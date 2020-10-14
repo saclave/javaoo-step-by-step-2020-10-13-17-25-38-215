7af76d671ab227067b2c6ee2b1c0dbb620aeb1e8
VEA MHAY JOY SACLAO (SHP-IRIS-ISD-OOCLL/MNL) <SACLAVE@oocl.com>
2020-10-15 00:18:25 +0800

    [JavaOOP][Vea] feature - practice11

--- src/main/java/practice11/Person.java
+++ src/main/java/practice11/Person.java
@@ -1,4 +1,46 @@
 package practice11;
 
+import java.util.Objects;
+
 public class Person {
+    private int id;
+    private String name;
+    private int age;
+
+    public Person(int id, String name, int age) {
+        this.name = name;
+        this.age = age;
+        this.id = id;
+    }
+
+    public Person() { }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getId() { return id; }
+
+    public String introduce(){
+        return "My name is " +name+ ". I am "+age+" years old.";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return id == person.id &&
+                age == person.age &&
+                Objects.equals(name, person.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, age);
+    }
 }
--- src/main/java/practice11/Teacher.java
+++ src/main/java/practice11/Teacher.java
@@ -1,4 +1,70 @@
 package practice11;
 
-public class Teacher {
+import java.util.LinkedList;
+
+import static CommonConstants.CommonConstants.TEACHER;
+
+public class Teacher extends Person{
+    public LinkedList<Klass> klasses;
+    private Klass klass;
+
+    public Teacher(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(int id, String name, int age, LinkedList<Klass> klasses) {
+        super(id, name, age);
+        this.klasses = klasses;
+    }
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+
+    public LinkedList getClasses(){
+        return klasses;
+    }
+
+    public String introduce(){
+        if(klasses == null){
+            return super.introduce() + TEACHER + " I teach No Class.";
+        }
+
+        return super.introduce() + TEACHER + " I teach Class "+iterateClasses()+".";
+    }
+
+    private String iterateClasses() {
+        String classes = "";
+
+        for(Klass klass : klasses){
+            if(klass == klasses.getLast()){
+                classes += klass.getNumber();
+                break;
+            }
+            classes += klass.getNumber() + ", ";
+        }
+
+        return classes;
+    }
+
+    public String introduceWith(Student student){
+        if(klasses.contains(student.getKlass())){
+            return super.introduce() + TEACHER + " I teach "+student.getName()+".";
+        }
+        else{
+            return super.introduce() + TEACHER + " I don't teach "+student.getName()+".";
+        }
+    }
+
+    public boolean isTeaching(Student student) {
+        return klasses.stream()
+                .anyMatch(stud -> stud.equals(student.getKlass()));
+    }
+
 }
